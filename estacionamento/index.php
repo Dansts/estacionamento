43422495686d38b5cc1dcd820495e83d59171a6a
Danilo Santos <danilosan_tos@globomail.com>
2016-03-07 22:03:29 -0300

    Inclusão de estilo na página inicial do projeto.

--- estacionamento/index.php
+++ estacionamento/index.php
@@ -3,8 +3,10 @@
 <head>
 	<meta charset="UTF-8" />
 	<title>Pagina Inicial do Projeto</title>
+	<link rel="stylesheet" href="bootstrap/min.css"/>
 </head>
 <body>
 	<h1>Pagina Inicial do Projeto</h1>
+	<?php print "Impresso em PHP";?>
 </body>
 </html>
