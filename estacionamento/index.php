2a1c173b1b92c6f85074253b5cca446ef1f4705d
Danilo Santos <danilosan_tos@globomail.com>
2016-03-07 21:54:41 -0300

    Estruturação da página inicial do projeto.

--- estacionamento/index.php
+++ estacionamento/index.php
@@ -1,3 +1,10 @@
-<?php
-
-	print "Pagina Inicial do Projeto";
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8" />
+	<title>Pagina Inicial do Projeto</title>
+</head>
+<body>
+	<h1>Pagina Inicial do Projeto</h1>
+</body>
+</html>
